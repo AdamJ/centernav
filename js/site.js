0774c7f9d1c3be04bb56e26b4b200b266ddddfb6
Adam Jolicoeur <mindreeper2420@gmail.com>
2018-03-22 11:29:23 -0400

    fix layouts and styles

--- js/site.js
+++ js/site.js
@@ -1,40 +1,11 @@
-$("#mega-menu--dropdown").click(function() {
-  $("#megaMenu").toggle("fast", function() {
-    // toggle the mega menu
-  });
-  $("#projectDetails").hide("fast", function () {
-    // hide project details
-  });
-  $("#megaMenuBase").show("fast", function () {
+$("#center-nav--dropdown").click(function() {
+  $("#centerNav").toggleClass("hide", function() {
+    // toggle the center menu
   });
 });
 
-$("#showProjectDetails").click(function() {
-  $("#megaMenuBase").hide("fast", function() {
-    // hide base mega menu content
+$("#center-nav--spaces-dropdown").click(function () {
+  $("#spacesDropdown").toggleClass("hide", function () {
+    // toggle the Spaces menu
   });
-  $("#projectDetails").show("fast", function () {
-    // show project details
-  });
-  $(".project-name").removeClass("hide");
-  $(".generic-space").addClass("hide");
-});
-$("#hideProjectDetails").click(function () {
-  $("#megaMenuBase").show("fast", function () {
-    // hide base mega menu content
-  });
-  $("#projectDetails").hide("fast", function () {
-    // show project details
-  });
-  $(".project-name").addClass("hide");
-  $(".generic-space").removeClass("hide");
-});
-
-$("#toggleNewFeature").click(function () {
-  $("#newFeature").toggleClass("hide"); // remove the .hide class if applied
-  $("#recentHighlights").toggleClass("hide"); // add the .hide class to show the newFeature block
-});
-
-$("#spaceMenuButton").click(function () {
-  $("#spaceMenu").toggleClass("hide-space-menu").toggleClass("show-space-menu");
 });
